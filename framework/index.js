019fab6bd2c899904b29dc5319174f4922dcd922
Emre YILMAZ <33000794+EmreRed@users.noreply.github.com>
2023-12-27 00:58:23 +0300

    Directory Layout Updated

--- framework/index.js
+++ framework/index.js
@@ -3,8 +3,8 @@
 global.Candy = {
     _config: require(`${__dir}/config.js`),
     Route  : require('./src/Route.js'),
-    Server : require('./src/Server.js'),
-    Mysql  : require('./src/Mysql.js')
+    Server : require('.src/Server.js'),
+    Mysql  : require('.src/Mysql.js')
 };
 
 module.exports = {
