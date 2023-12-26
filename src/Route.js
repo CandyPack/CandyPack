3dc421ca9e2779dbde9675c5b4fbc9dfc2e7e999
Emre YILMAZ <33000794+EmreRed@users.noreply.github.com>
2023-12-27 00:37:49 +0300

    Custom Classes Supported

--- src/Route.js
+++ src/Route.js
@@ -6,10 +6,13 @@ var routes = {};
 
 function set(type, url, file){
     if(url.substr(-1) === '/') url = url.substr(0, url.length - 1);
-    let path = `${__dir}/controller/${type}/${file}.js`;
-    if(file.includes('.')){
-        let arr = file.split('.');
-        path = `${__dir}/controller/${arr[0]}/${type}/${arr.slice(1).join('.')}.js`;
+    let path = `${__dir}/route/${Candy.Route.buff}.js`;
+    if(typeof file !== 'function'){
+        path = `${__dir}/controller/${type}/${file}.js`;
+        if(file.includes('.')){
+            let arr = file.split('.');
+            path = `${__dir}/controller/${arr[0]}/${type}/${arr.slice(1).join('.')}.js`;
+        }
     }
     if(!Candy.Route.routes[Candy.Route.buff]) Candy.Route.routes[Candy.Route.buff] = {};
     if(!Candy.Route.routes[Candy.Route.buff][type]) Candy.Route.routes[Candy.Route.buff][type] = {};
@@ -22,7 +25,8 @@ function set(type, url, file){
     }
     if(fs.existsSync(path)){
         if(!Candy.Route.routes[Candy.Route.buff][type][url]) Candy.Route.routes[Candy.Route.buff][type][url] = {};
-        Candy.Route.routes[Candy.Route.buff][type][url].cache = require(path);
+        Candy.Route.routes[Candy.Route.buff][type][url].cache = typeof file === 'function' ? file : require(path);
+        Candy.Route.routes[Candy.Route.buff][type][url].type = typeof file === 'function' ? 'function' : 'controller';
         Candy.Route.routes[Candy.Route.buff][type][url].file = file;
         Candy.Route.routes[Candy.Route.buff][type][url].mtime = fs.statSync(path).mtimeMs;
         Candy.Route.routes[Candy.Route.buff][type][url].reload = reload;
@@ -33,6 +37,22 @@ function set(type, url, file){
 function init(){
     if(loading) return;
     loading = true;
+    for(const file of fs.readdirSync(`${__dir}/controller/`)){
+        if(file.substr(-3) !== '.js') continue;
+        let name = file.replace('.js', '');
+        if(!Candy.Route.class) Candy.Route.class = {};
+        if(Candy.Route.class[name]){
+            if(Candy.Route.class[name].mtime >= fs.statSync(Candy.Route.class[name].path).mtimeMs) continue;
+            delete global[name];
+            delete require.cache[require.resolve(Candy.Route.class[name].path)];
+        }
+        if(global[name]) continue;
+        Candy.Route.class[name] = {
+            path: `${__dir}/controller/${file}`,
+            mtime: fs.statSync(`${__dir}/controller/${file}`).mtimeMs
+        };
+        global[name] = require(Candy.Route.class[name].path);
+    }
     let dir = fs.readdirSync(`${__dir}/route/`);
     for(const file of dir){
         if(file.substr(-3) !== '.js') continue;
@@ -52,6 +72,7 @@ function init(){
                     delete Candy.Route.routes[Candy.Route.buff][type][route];
                     delete require.cache[require.resolve(`${__dir}/controller/page/${route}.js`)];
                 } else if(Candy.Route.routes[Candy.Route.buff][type][route]){
+                    if(typeof Candy.Route.routes[Candy.Route.buff][type][route].type === 'function') continue;
                     if(Candy.Route.routes[Candy.Route.buff][type][route].mtime < fs.statSync(Candy.Route.routes[Candy.Route.buff][type][route].path).mtimeMs){
                         set(type, route, Candy.Route.routes[Candy.Route.buff][type][route].file);
                     }
@@ -69,21 +90,21 @@ module.exports = {
         init();
         setInterval(init, 1000);
     },
-    request: function(req, res){
+    request: async function(req, res){
         let route = req.headers.host.split('.')[0];
         let url = req.url.split('?')[0];
         if(url.substr(-1) === '/') url = url.substr(0, url.length - 1);
         let type = req.method.toLowerCase();
-        console.log(route, type, url);
-        if(Candy.Route.routes[route]){
-            if(Candy.Route.routes[route][type] && Candy.Route.routes[route][type][url]) Candy.Route.routes[route][type][url].cache(req, res);
-            else res.end('no');
-        } else if(Candy.Route.routes['www']){
-            if(Candy.Route.routes['www'][type] && Candy.Route.routes['www'][type][url]) Candy.Route.routes['www'][type][url].cache(req, res);
-            else res.end('no');
-        } else {
-            res.end();
+        if(!Candy.Route.routes[route]) route = 'www';
+        if(!Candy.Route.routes[route]) return res.end();
+        let result = null;
+        if(Candy.Route.routes[route][type] && Candy.Route.routes[route][type][url]) result = await Candy.Route.routes[route][type][url].cache();
+        else if(Candy.Route.routes[route].error && Candy.Route.routes[route].error[404]) result = await Candy.Route.routes[route].error[404].cache();
+        if(result){
+            result = JSON.stringify(typeof result === 'object' ? result : { result: result });
+            res.writeHead(200, { 'Content-Type': 'application/json' });
         }
+        res.end(result);
     },
     page: function(path, file){
         set('page', path, file);
