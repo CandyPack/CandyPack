60c8008e7561b75f5b879c453e06dda0aadedb1f
Emre <33000794+EmreRed@users.noreply.github.com>
2025-09-04 16:55:03 +0300

    Fix default value for websites in stopAll method
    Replaces the default value for websites from an empty array to an empty object in the stopAll method to prevent potential issues when iterating over website domains.

--- core/Process.js
+++ core/Process.js
@@ -17,7 +17,7 @@ class Process {
   async stopAll() {
     if (Candy.core('Config').config.server?.watchdog) await this.stop(Candy.core('Config').config.server.watchdog)
     if (Candy.core('Config').config.server?.pid) await this.stop(Candy.core('Config').config.server.pid)
-    for (const domain of Object.keys(Candy.core('Config').config?.websites ?? []))
+    for (const domain of Object.keys(Candy.core('Config').config?.websites ?? {}))
       if (Candy.core('Config').config.websites[domain].pid) await this.stop(Candy.core('Config').config.websites[domain].pid)
     for (const service of Candy.core('Config').config.services ?? []) if (service.pid) await this.stop(service.pid)
   }
