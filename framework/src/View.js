20d061272b78683494fdb1b71f7518f69eaecc86
Emre <33000794+EmreRed@users.noreply.github.com>
2025-10-14 16:34:14 +0300

    Refactor cache directory usage in View.js
    Introduced CACHE_DIR constant to centralize cache directory path and updated all references to use it. This improves maintainability and reduces hardcoded strings.

--- framework/src/View.js
+++ framework/src/View.js
@@ -1,6 +1,8 @@
 const nodeCrypto = require('crypto')
 const fs = require('fs')
 
+const CACHE_DIR = './storage/.cache'
+
 class View {
   #cache = {}
   #functions = {
@@ -224,12 +226,9 @@ class View {
         }
       }
       let cache = `${nodeCrypto.createHash('md5').update(file).digest('hex')}`
-      if (!fs.existsSync('./storage/.cache')) fs.mkdirSync('./storage/.cache', {recursive: true})
-      fs.writeFileSync(
-        './storage/.cache/' + cache,
-        `module.exports = (Candy, get, __) => {\nlet html = '';\n${result}\nreturn html.trim()\n}`
-      )
-      delete require.cache[require.resolve(__dir + '/storage/.cache/' + cache)]
+      if (!fs.existsSync(CACHE_DIR)) fs.mkdirSync(CACHE_DIR, {recursive: true})
+      fs.writeFileSync(`${CACHE_DIR}/${cache}`, `module.exports = (Candy, get, __) => {\nlet html = '';\n${result}\nreturn html.trim()\n}`)
+      delete require.cache[require.resolve(`${__dir}/${CACHE_DIR}/${cache}`)]
       if (!Candy.View) Candy.View = {}
       if (!Candy.View.cache) Candy.View.cache = {}
       Candy.View.cache[file] = {
@@ -238,7 +237,7 @@ class View {
       }
     }
     try {
-      return require(__dir + '/storage/.cache/' + Candy.View.cache[file].cache)(
+      return require(`${__dir}/${CACHE_DIR}/${Candy.View.cache[file].cache}`)(
         this.#candy,
         key => this.#candy.Request.get(key),
         (...args) => this.#candy.Lang.get(...args)
